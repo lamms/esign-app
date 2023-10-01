abc87139859d4c9527a84625f537c104aa3405ca
lamms <sonlam.gun@gmail.com>
2023-10-01 10:55:14 +0700

    LamMS Commit

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+import {registerLicense} from '@syncfusion/ej2-base';
+registerLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdgWXZceXVTQmhdU0ZxXUs=");
 if (environment.production) {
   enableProdMode();
 }
